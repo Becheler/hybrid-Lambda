27aa6598ecfc43ce8e93463426a07893b6f75e6a
Arnaud Becheler <arnaud.becheler@gmail.com>
2022-06-16 14:42:18 -0400

    fix: cleanup

--- tests/unittest/test_hybridLambda.cpp
+++ tests/unittest/test_hybridLambda.cpp
@@ -1,11 +1,11 @@
 /*
- * hybrid-Lambda is used to simulate gene trees given species network under 
+ * hybrid-Lambda is used to simulate gene trees given species network under
  * coalescent process.
- * 
+ *
  * Copyright (C) 2010 -- 2014 Sha (Joe) Zhu
- * 
+ *
  * This file is part of hybrid-Lambda.
- * 
+ *
  * hybrid-Lambda is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -15,47 +15,86 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
-#include <cppunit/TestCase.h>
-#include <cppunit/extensions/HelperMacros.h>
+
+#define BOOST_TEST_MODULE TestHybridLambda
+
+#include <boost/test/unit_test.hpp>
+
 #include "src/hybridLambda.hpp"
-#pragma GCC diagnostic ignored "-Wwrite-strings"
+#include "src/sim_gt.hpp"
 
-class TestHybirdLambda : public CppUnit::TestCase {
-  
-    CPPUNIT_TEST_SUITE( TestHybirdLambda );
-    
-    CPPUNIT_TEST( test_CMD ); 
-    //CPPUNIT_TEST( bad_CMD ); 
+BOOST_AUTO_TEST_SUITE( assert_not_throws )
 
-    CPPUNIT_TEST_SUITE_END();
+BOOST_AUTO_TEST_CASE( test_command_line_interface )
+{
+  char* argv1[] = { "hybrid-Lambda", "-spcu", "trees/7_tax_sp_nt1_para", "-seg", "-o", "blah", "-sim_mut_unit", "-sim_num_gener" };
 
- private:
+  BOOST_CHECK_NO_THROW( HybridLambda( 8, argv1 ) );
 
-    void test_CMD() {
-        char* argv1[] = { "hybrid-Lambda", "-spcu", "trees/7_tax_sp_nt1_para", "-seg", "-o", "blah", "-sim_mut_unit", "-sim_num_gener" };
-        CPPUNIT_ASSERT_NO_THROW ( HybridLambda( 8, argv1 ) ); 
-        CPPUNIT_ASSERT_NO_THROW ( HybridLambda( 7, argv1 ) ); 
-        CPPUNIT_ASSERT_NO_THROW ( HybridLambda( 6, argv1 ) );
-        CPPUNIT_ASSERT_NO_THROW ( HybridLambda( 4, argv1 ) );
-        CPPUNIT_ASSERT_NO_THROW ( HybridLambda( 3, argv1 ) );
+  BOOST_CHECK_NO_THROW( HybridLambda( 7, argv1 ) );
 
-        
-        CPPUNIT_ASSERT_THROW ( HybridLambda( 5, argv1 ) , std::invalid_argument ); 
-        CPPUNIT_ASSERT_THROW ( HybridLambda( 2, argv1 ) , std::invalid_argument ); 
-        //char* argv2[] = { "hybrid-Lambda", "-spcu", "trees/7_tax_sp_nt1_para", "-dotF", "blah", "-label", "-branch" };
-        //CPPUNIT_ASSERT_NO_THROW ( HybridLambda( 6, argv1 ) ); 
-        //CPPUNIT_ASSERT_NO_THROW ( HybridLambda( 5, argv1 ) );        
-    }
-    
-    //void bad_CMD(){
-        //char* argv1[] = { "hybrid-Lambda", "-spcu", "../trees/7_tax_sp_nt1_para", "-dot", "-o", "blah", "-branch", "-label" };
+  BOOST_CHECK_NO_THROW( HybridLambda( 6, argv1 ) );
 
-    //}
-};
+  BOOST_CHECK_NO_THROW( HybridLambda( 4, argv1 ) );
 
-CPPUNIT_TEST_SUITE_REGISTRATION( TestHybirdLambda );
+  BOOST_CHECK_NO_THROW( HybridLambda( 3, argv1 ) );
+
+  BOOST_CHECK_NO_THROW( HybridLambda( 5, argv1 ) , std::invalid_argument );
+
+  BOOST_CHECK_NO_THROW( HybridLambda( 2, argv1 ) , std::invalid_argument );
+}
+
+BOOST_AUTO_TEST_CASE( test_runner )
+{
+  //if p = 1: lambda_{b, b}  =  1,  and all other lambda_{b,k} are 0
+  BOOST_ASSERT_EQUAL(1.0, testSubjectPtr->lambdaPsi(2, 2, 1) );
+
+  //CPPUNIT_ASSERT_ASSERTION_FAIL( testSubjectPtr->lambdaPsi(2, 1, 1) ); // This should not happen, k should be always at least 2.
+  //CPPUNIT_ASSERT_ASSERTION_FAIL( testSubjectPtr->lambdaPsi(2, 3, 1) );
+
+  BOOST_ASSERT_EQUAL(1.0, testSubjectPtr->lambdaPsi(3, 3, 1) );
+
+  BOOST_ASSERT_EQUAL(0.0, testSubjectPtr->lambdaPsi(3, 2, 1) );
+
+  BOOST_ASSERT_EQUAL(1.0, testSubjectPtr->lambdaPsi(30, 30, 1) );
+
+  BOOST_ASSERT_EQUAL(0.0, testSubjectPtr->lambdaPsi(30, 10, 1) );
+
+  BOOST_ASSERT_EQUAL(0.0, testSubjectPtr->lambdaPsi(30, 3, 1) );
+
+  BOOST_ASSERT_EQUAL(1.0, testSubjectPtr->lambdaPsi(100, 100, 1) );
+
+  BOOST_ASSERT_EQUAL(0.0, testSubjectPtr->lambdaPsi(100, 99, 1) );
+
+  BOOST_ASSERT_EQUAL(0.0, testSubjectPtr->lambdaPsi(100, 97, 1) );
+
+  BOOST_ASSERT_EQUAL(0.0, testSubjectPtr->lambdaPsi(100, 93, 1) );
+
+  //if  p = 0:  lambda_{b, 2} =  b*(b-1)/2, and all other lambda_{b,k} are 0
+  BOOST_ASSERT_EQUAL(0.0, testSubjectPtr->lambdaPsi(3, 3, 0) );
+
+  BOOST_ASSERT_EQUAL(3, (int)testSubjectPtr->lambdaPsi(3, 2, 0) );
+
+  BOOST_ASSERT_EQUAL(0.0, testSubjectPtr->lambdaPsi(4, 4, 0) );
+
+  BOOST_ASSERT_EQUAL(0.0, testSubjectPtr->lambdaPsi(4, 3, 0) );
+
+  BOOST_ASSERT_EQUAL(6, (int)testSubjectPtr->lambdaPsi(4, 2, 0) );
+
+  BOOST_ASSERT_EQUAL(0.0, testSubjectPtr->lambdaPsi(40, 40, 0) );
+
+  BOOST_ASSERT_EQUAL(0.0, testSubjectPtr->lambdaPsi(40, 39, 0) );
+
+  BOOST_ASSERT_EQUAL(0.0, testSubjectPtr->lambdaPsi(40, 20, 0) );
+
+  BOOST_ASSERT_EQUAL(0.0, testSubjectPtr->lambdaPsi(40, 9, 0) );
+
+  BOOST_ASSERT_EQUAL(780, (int)testSubjectPtr->lambdaPsi(40, 2, 0) );
+}
+
+BOOST_AUTO_TEST_SUITE_END()
